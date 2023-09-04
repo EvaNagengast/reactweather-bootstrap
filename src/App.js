63b9b809712fe2bc410b6141a1eea0c661413aaf
SpiceWeasel66 <nagengasteva@gmail.com>
2023-09-04 16:17:38 +0100

    added basic footer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,31 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
- <h1>hello</h1>
+      <h1>hello</h1>
+      <footer>
+        <p>
+          Coded with love by{" "}
+          <a
+            href="https://www.linkedin.com/in/eva-nagengast-9b78b220a"
+            className="contactlink"
+            title="personal linkedin profile"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Eva Nagengast
+          </a>{" "}
+          | This project is open sourced on{" "}
+          <a
+            href="https://github.com/EvaNagengast/reactweather-bootstrap"
+            className="gitHub"
+            title="Git hub contact"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
