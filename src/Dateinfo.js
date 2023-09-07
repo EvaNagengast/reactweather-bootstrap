b1a61287be814b3d3cda19032d61862ea0f3e380
SpiceWeasel66 <nagengasteva@gmail.com>
2023-09-07 16:01:54 +0100

    added display date

--- src/Dateinfo.js
+++ src/Dateinfo.js
@@ -1,16 +1,42 @@
 import React from "react";
 
 export default function Dateinfo() {
-let currentDate= new Date()
-  let today = currentDate.getDate();
-  let displayday = `${today}, `;
-
-  console.log(displayday);
-
+  let currentDate = new Date();
+  let todayweekday = currentDate.getDay();
+  let todaydate = currentDate.getDate();
+  let todaymonth = currentDate.getMonth();
+  let todayyear = currentDate.getFullYear();
+  let todayshort = todayyear - 2000;
+  const dayname = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const monthname = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let displayday = dayname[todayweekday];
+  let displaymonth = monthname[todaymonth];
   return (
     <div>
-      <p>Current Day of the Month:</p>
-      <p>{displayday}</p>
+      <p>
+        {displayday}, {todaydate}.{displaymonth}, `{todayshort}
+      </p>
     </div>
   );
 }
