1f3f58e493ca02ad7f5567750565c971eab749b7
SpiceWeasel66 <nagengasteva@gmail.com>
2023-09-08 12:59:35 +0100

    changes background added forecast icons

--- src/Weathericon.js
+++ src/Weathericon.js
@@ -1,10 +1,12 @@
 import React from "react";
 import sun from "./media/clearskyday.png";
 import moon from "./media/clearskynight.png";
+import cloud from "./media/cloud.png";
 import clouds from "./media/clouds.png";
+import shower from "./media/shower.png";
 import fewcloudsd from "./media/fewcloudsday.png";
 import fewcloudsn from "./media/fewcloudsnight.png";
-import mist from "./media/mist.png";
+import fog from "./media/fog.png";
 import rain from "./media/rain.png";
 import snow from "./media/snow.png";
 import storm from "./media/storm.png";
@@ -16,20 +18,20 @@ export default function WeatherIcon(props) {
     "clear-sky-night": moon,
     "few-clouds-day": fewcloudsd,
     "few-clouds-night": fewcloudsn,
-    "scattered-clouds-day": clouds,
-    "scattered-clouds-night": clouds,
+    "scattered-clouds-day": cloud,
+    "scattered-clouds-night": cloud,
     "broken-clouds-day": clouds,
     "broken-clouds-night": clouds,
-    "shower-rain-day": rain,
-    "shower-rain-night": rain,
+    "shower-rain-day": shower,
+    "shower-rain-night": shower,
     "rain-day": rain,
     "rain-night": rain,
     "thunderstorm-day": storm,
     "thunderstorm-night": storm,
     "snow-day": snow,
     "snow-night": snow,
-    "mist-day": mist,
-    "mist-night": mist,
+    "mist-day": fog,
+    "mist-night": fog,
   };
 
   const weatherImage = codeMapping[props.icon];
