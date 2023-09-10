6cbe291e5adb7029b96a317dcf2b9903c11cfcd2
SpiceWeasel66 <nagengasteva@gmail.com>
2023-09-10 12:48:02 +0100

    update forecast at search

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,36 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import ForecastDayDisplay from "./ForecastDayDisplay";
-
-import ForecastIcon from "./ForecastIcon";
-
-
 
 import "./Forecast.css";
 export default function Forecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.info]);
+
   function handleResponse(response) {
     setForecast(response.data.daily);
-
     setLoaded(true);
   }
 
   if (loaded) {
-//   console.log(forecast[2])
     return (
-         
-      <div className="Forecast mt-4">
-        <ForecastDayDisplay info={forecast[1]} />
-      
-        <div className="forecasticon">
-          <ForecastIcon icon={forecast} />
+      <div className="Forecast  mt-4">
+   
+        <div className="row">
+          <div className="col">
+            <ForecastDayDisplay info={forecast[1]} />
+          </div> 
+          <div className="col">
+            <ForecastDayDisplay info={forecast[2]} />
+          </div> 
+          <div className="col">
+            <ForecastDayDisplay info={forecast[3]} />
+          </div> 
+          <div className="col">
+            <ForecastDayDisplay info={forecast[4]} />
+          </div> 
+          <div className="col">
+            <ForecastDayDisplay info={forecast[5]} />
+          </div>
         </div>
       </div>
     );
   } else {
     let city = props.info;
-
     let apiKey = "5101b1tb3fba4e5cedfo0b346a6ccc32";
     let units = "metric";
     let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=${units}`;
--- src/ForecastDayDisplay.js
+++ src/ForecastDayDisplay.js
@@ -1,22 +1,53 @@
 import React from "react";
+import forecastsun from "./media/forecastclearskyday.png";
+import forecastcloud from "./media/forecastcloud.png";
+import forecastclouds from "./media/forecastclouds.png";
+import forecastshower from "./media/forecastshower.png";
+import forecastfewclouds from "./media/forecastfewcloudsday.png";
+import forecastfog from "./media/forecastfog.png";
+import forecastrain from "./media/forecastrain.png";
+import forecastsnow from "./media/forecastsnow.png";
+import forecaststorm from "./media/forecaststorm.png";
+
 import "./ForecastDayDisplay.css";
 
 export default function ForecastDayDisplay(props) {
-  console.log(props);
   const dayname = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastTime = new Date(props.info.time * 1000);
   let forecastNextDay = forecastTime.getDay();
   let forecastWeekday = dayname[forecastNextDay];
 
+  //
+
   let maxiTemp = Math.round(props.info.temperature.maximum);
   let miniTemp = Math.round(props.info.temperature.minimum);
-  //    console.log(currentTime);
+
+  //
+
+  const codeMapping = {
+    "clear-sky-day": forecastsun,
+    "few-clouds-day": forecastfewclouds,
+    "scattered-clouds-day": forecastcloud,
+    "broken-clouds-day": forecastclouds,
+    "shower-rain-day": forecastshower,
+    "rain-day": forecastrain,
+    "thunderstorm-day": forecaststorm,
+    "snow-day": forecastsnow,
+    "mist-day": forecastfog,
+  };
+  let icondescription = props.info.condition.icon;
+
+  let iconpath = codeMapping[icondescription];
+
   return (
     <div className="ForecastDayDisplay">
       <div className="forecastWeekday">{forecastWeekday}</div>
       <div className="forecastTemperature">
         <span className="maximumTemperature">{maxiTemp}°</span>
         <span className="minimumTemperature">{miniTemp}°</span>
+      </div>
+      <div className="forecastIcon">
+        <img src={iconpath} alt="forecast icon" />
       </div>
     </div>
   );
